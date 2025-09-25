f81c3a76d9dad79e1c5e24d06cce7e009ccf79d6
fatihamaheswarifitriandita-pixel <fatihamaheswarifitriandita@mail.ugm.ac.id>
2025-09-25 11:21:55 +0700

    hide menu promo dari header

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -268,7 +268,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#hubungi">Hubungi</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item" style="display: none;">
                             <a class="nav-link" href="#promo">Promo</a>
                         </li>
                         <li class="nav-item">
