565038dfb37f9fb758b621cf582d008954bf10bb
alpiyaaa <158010769+alpiyaaa@users.noreply.github.com>
2025-09-30 10:53:02 +0700

    mbuh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Siswa Baru TA 2024-2024</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Daftar Siswa Baru TA 2024-2025</a>
+            @php
+                $tahunAwal = now()->year;
+                $tahunAkhir = $tahunAwal + 1;
+            @endphp
+
+            <h5 class="fs-5 fw-bold text-white m-0">
+                Daftar Siswa Baru TA {{ $tahunAwal }}-{{ $tahunAkhir }}
+            </h5>
         </div>
     </nav>
 
